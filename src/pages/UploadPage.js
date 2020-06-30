a9a339da537a7273c8b4dc097f6349d8fa69ffb2
Michael Dodge <Michael.Dodge@lfg.com>
2020-06-30 12:25:32 -0400

    savvy url change

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -103,7 +103,7 @@ class Upload extends Component {
       
       console.log('Your input value is: ' + this.state.savvyPath)
 
-      var savvyKey = this.state.savvyPath.replace("https://savvyanalysis.com/flight/", "");
+      var savvyKey = this.state.savvyPath.replace("https://apps.savvyaviation.com/flights/", "");
       req.open("POST", "https://garmin-conversion-service-5fjvdhip2a-uc.a.run.app/combine?startingFuel="+this.state.startingFuel+"&savvyFlight="+savvyKey+"&jpiSecondsOffset="+this.state.jpiOffset);
       req.setRequestHeader('Content-Disposition', 'attachment; filename="filename.csv" filename*="filename.csv"')
       req.send(formData);
